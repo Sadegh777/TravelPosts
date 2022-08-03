5f4c4233fb3fb420b2dd68fc239affe01469e3d0
russelldnn <russelldnn@gmail.com>
2022-08-03 09:21:14 -0500

    added title

--- client/src/components/MemoryList/index.js
+++ client/src/components/MemoryList/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useMutation } from '@apollo/client';
-import { REMOVE_MEMORY } from '../../utils/mutations';
-import { QUERY_ME } from '../../utils/queries';
+// import { useMutation } from '@apollo/client';
+// import { REMOVE_MEMORY } from '../../utils/mutations';
+// import { QUERY_ME } from '../../utils/queries';
 
 const MemoryList = ({
   memories,
@@ -13,28 +13,28 @@ const MemoryList = ({
   if (!memories.length) {
     return <h3>Add a memory to Recall</h3>;
   }
-  const [removeMemory, {error}] = useMutation(REMOVE_MEMORY, {
-    update(cache, {data: {removeMemory}}) {
-      try {
-        cache.writeQuery({
-          query: QUERY_ME,
-          data: { me:removeMemory},
-        });
-      } catch (e) {
-        console.error(e);
-      }
-    },
-  });
+  // const [removeMemory, {error}] = useMutation(REMOVE_MEMORY, {
+  //   update(cache, {data: {removeMemory}}) {
+  //     try {
+  //       cache.writeQuery({
+  //         query: QUERY_ME,
+  //         data: { me:removeMemory},
+  //       });
+  //     } catch (e) {
+  //       console.error(e);
+  //     }
+  //   },
+  
 
-  const handleRemoveMemory = async (memory) => {
-    try {
-      const { data } = await removeMemory({
-        variables: { memory},
-      });
-    } catch (err) {
-      console.error(err);
-    }
-  };
+  // const handleRemoveMemory = async (memory) => {
+  //   try {
+  //     const { data } = await removeMemory({
+  //       variables: { memory},
+  //     });
+  //   } catch (err) {
+  //     console.error(err);
+  //   }
+  // };
 
   return (
     <div>
@@ -62,25 +62,25 @@ const MemoryList = ({
               )}
             </h4>
             <div className="card-body bg-light p-2">
-              <p>{memory.memoryTitle}</p> <br/>
+              <h1>{memory.memoryTitle}</h1> <br/>
               <p>{memory.memoryText}</p>
             </div>
-            <button
+            {/* <button
               onClick={() => handleRemoveMemory(memory)}
               >
                 x
-              </button>
+              </button> */}
             <Link
               className="btn btn-primary btn-block btn-squared"
               to={`/memories/${memory._id}`}
             >
               Comment on this memory
             </Link>
-            {error && (
+            {/* {error && (
             <div className="col-12 my-3 bg-danger text-white p-3">
               {error.message}
             </div>
-          )}
+          )} */}
           </div>
         ))}
     </div>
