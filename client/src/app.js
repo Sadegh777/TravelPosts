20c57ab23a7c3f9588ed8ee18ef7daf3f77f2302
russelldnn <russelldnn@gmail.com>
2022-08-03 11:12:11 -0500

    heroku changes

--- client/src/app.js
+++ client/src/app.js
@@ -11,7 +11,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import Signup from './pages/Signup';
 import Login from './pages/login';
-import SingleMemory from './pages/Memory'
+import SingleMemory from './pages/memory'
 import User from './pages/User';
 import Header from './components/Header';
 import Footer from './components/Footer';
